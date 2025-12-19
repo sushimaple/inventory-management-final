96a0f1b8147930d014cdb01f72af9bc21eb63db5
aaron <aaronsamonte02@gmail.com>
2025-12-19 18:13:04 +0800

    feat: integrate Prisma with PostgreSQL and add user/post models
    - Updated package.json to include Prisma and PostgreSQL dependencies.
    - Configured Prisma with a new schema defining User and Post models.
    - Added migration files for initial database setup.
    - Implemented a seed script to populate the database with sample users and posts.
    - Created StackHandler and SignIn components for user authentication.
    - Added loading state handling in the application.
    - Introduced MCP configuration files for server integration.

---
+++ app/sign-in/page.tsx
@@ -0,0 +1,14 @@
+import { SignIn } from "@stackframe/stack";
+import Link from "next/link";
+
+
+export default function SignInPage() {
+    return(
+        <div className="min-h-screen flex items-center justify-center bg-linear-to-br from-purple-50 to-puprle-100">
+            <div className="max-w-md w-full space-y-8">
+                <SignIn/>
+                <Link href="/">Home</Link>
+            </div>
+        </div>
+    )
+}
