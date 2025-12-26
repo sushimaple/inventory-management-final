8c8d2797bc2dd063e6032c5a0ca1464aa896ec57
Aaron David <aaronsamonte02@gmail.com>
2025-12-26 14:54:20 +0800

    feat: implement ProductsChart component and add recharts dependency

--- components/products-chart.tsx
+++ components/products-chart.tsx
@@ -1,10 +1,24 @@
+"use client";
+
+import { AreaChart, CartesianGrid, ResponsiveContainer } from "recharts";
+
 interface ChartData {
   week: string;
   products: number;
 }
 
 export default function ProductsChart({ data }: { data: ChartData[] }) {
-  return <div></div>;
-
   console.log(data);
+  return (
+    <div className="h-48 w-full ">
+      <ResponsiveContainer width="100%" height="100%">
+        <AreaChart
+          data={data}
+          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+        >
+          <CartesianGrid strokeDasharray="3 3" stroke="#f0f0f0" />
+        </AreaChart>
+      </ResponsiveContainer>
+    </div>
+  );
 }
